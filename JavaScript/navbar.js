32ed0d59ea3e98465fb359f56bac0d174e925623
Nishi Tiwari <nishit5195@gmail.com>
2024-05-01 18:03:09 +0530

    Here and There

---
+++ JavaScript/navbar.js
@@ -0,0 +1,8 @@
+// navbar.js
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('navbar-placeholder').innerHTML = html;
+        });
+});
